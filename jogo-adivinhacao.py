3ba0df790946a02c45e5a90015944d658e4bf5b0
rafael <rafaelcamargodeoliveira@hotmail.com>
2023-03-10 21:04:21 -0300

    Correção de bugs na execução do laço while

--- jogo-adivinhacao.py
+++ jogo-adivinhacao.py
@@ -1,9 +1,24 @@
 numero_secreto = 42
-chute_str = input('Digite o seu número: ')
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(chute_str)
+while rodada <= total_de_tentativas:
+    print(f"tentativa {rodada} de {total_de_tentativas}")
 
-if numero_secreto == chute:
-    print('Parabéns. Você acertou o número!')
-else:
-    print('você errou o número')
+    chute_str = input('Digite o seu número: ')
+    print(f'Você digitou {chute_str}')
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if acertou:
+        print('Parabéns. Você acertou!')
+    else:
+        if maior:
+            print('O seu chute foi maior do que o número secreto!')
+        if menor:
+            print('O seu chute foi menor do que o número secreto!')
+    
+    rodada = rodada + 1
