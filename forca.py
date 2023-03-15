3e41ecde01bd1a9715b7134aa5f49434ec43d458
rafael <rafaelcamargodeoliveira@hotmail.com>
2023-03-15 18:52:13 -0300

    Criação do jogo forca - Primeiros passos - Criando laços

--- forca.py
+++ forca.py
@@ -1,5 +1,31 @@
 def jogar():
-    print('jogo de forca - Em construção')
+    print('***********************************')
+    print('*****Bem vindo ao jogo forca!******')
+    print('***********************************')
+    print('Desenvolvido por Rafael C. Oliveira')
+
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        
+        chute = input('Qual letra você quer tentar? ')
+        chute = chute.strip()
+        
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                print(f'Encontrei {letra} na posição {index}')
+            index = index + 1
+        
+        print('jogando...')
+
+        
+
+
 
 if (__name__ == '__main__'):
     jogar()
